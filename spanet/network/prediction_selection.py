f6e501577ab8c29d128047fbd58fccbdddb3c546
ereinha <74273926+ereinha@users.noreply.github.com>
2023-08-09 12:45:20 -0500

    Update prediction_selection.py

--- spanet/network/prediction_selection.py
+++ spanet/network/prediction_selection.py
@@ -272,9 +272,7 @@ def extract_predictions(predictions: List[TArray]):
     for i in prange(batch_size):
         temp_weight = weights[:,i,:]
         new_product = np.prod(np.exp(temp_weight), axis=0)
-        print('temp_weight: ', temp_weight)
         indx = np.argmax(new_product)
-        print('new_product: ', new_product)
         max_results[:,i,:] = results[:,i,:,indx]
             
     return [max_result[:, :partons] for max_result, partons in zip(max_results, num_partons)]
