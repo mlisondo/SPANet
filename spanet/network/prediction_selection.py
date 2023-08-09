c84f3a4aed3ff9ac23a9ffacc979ac6732858229
ereinha <74273926+ereinha@users.noreply.github.com>
2023-08-09 00:26:53 -0500

    Update prediction_selection.py

--- spanet/network/prediction_selection.py
+++ spanet/network/prediction_selection.py
@@ -263,9 +263,9 @@ def extract_predictions(predictions: List[TArray]):
         for l in range(targets):
             for m in range(batch_size):
                 current_tuple = tuple(result[l, m])
-                # print(current_tuple)
-                # print(valid_perms)
-                # print(type(current_tuple[0]), type(valid_perms[0][0]))
+                print(current_tuple)
+                print(valid_perms)
+                print(type(current_tuple[0]), type(valid_perms[0][0]))
                 if tuple(result[l, m]) in valid_perms:
                     print('True')
                     weights[j, m, l] = original_weights[m, result[l,m,0], result[l,m,1], result[l,m,2]]
