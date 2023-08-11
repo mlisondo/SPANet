f575483586f1ccbed5d2df99efd43541902dd733
ereinha <74273926+ereinha@users.noreply.github.com>
2023-08-11 16:39:18 -0500

    Update evaluation.py

--- spanet/evaluation.py
+++ spanet/evaluation.py
@@ -151,7 +151,7 @@ def evaluate_on_test_dataset(
         }
 
         assignment_probabilities = []
-        print(outputs.assignments.shape)
+        print(outputs.assignments[0].shape)
         dummy_index = torch.arange(assignment_indices[0].shape[0]).unsqueeze(-1).repeat(1, assignment_indices[0].shape[-1])
         for assignment_probability, assignment, symmetries in zip(
             outputs.assignments,
