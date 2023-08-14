236f7ca673805aa0c9ee291a592f94c458f0d31d
ereinha <74273926+ereinha@users.noreply.github.com>
2023-08-14 13:22:31 -0500

    Update prediction_selection.py

--- spanet/network/prediction_selection.py
+++ spanet/network/prediction_selection.py
@@ -73,6 +73,47 @@ def mask_3(flat_data, size, index, value):
 #
 # @njit("void(float32[::1], int64, int64, float32)")
 # def mask_6(flat_data, size, index, value):
+#     data = flat_data.reshape((size, size, size, size, size, size))
+#     data[index, :, :, :, :, :] = value
+#     data[:, index, :, :, :, :] = value
+#     data[:, :, index, :, :, :] = value
+#     data[:, :, :, index, :, :] = value
+#     data[:, :, :, :, index, :] = value
+#     data[:, :, :, :, :, index] = value
+
+
+# @njit("void(float32[::1], int64, int64, float32)")
+# def mask_7(flat_data, size, index, value):
+#     data = flat_data.reshape((size, size, size, size, size, size, size))
+#     data[index, :, :, :, :, :, :] = value
+#     data[:, index, :, :, :, :, :] = value
+#     data[:, :, index, :, :, :, :] = value
+#     data[:, :, :, index, :, :, :] = value
+#     data[:, :, :, :, index, :, :] = value
+#     data[:, :, :, :, :, index, :] = value
+#     data[:, :, :, :, :, :, index] = value
+#
+#
+# @njit("void(float32[::1], int64, int64, float32)")
+# def mask_8(flat_data, size, index, value):
+#     data = flat_data.reshape((size, size, size, size, size, size, size, size))
+#     data[index, :, :, :, :, :, :, :] = value
+#     data[:, index, :, :, :, :, :, :] = value
+#     data[:, :, index, :, :, :, :, :] = value
+#     data[:, :, :, index, :, :, :, :] = value
+#     data[:, :, :, :, index, :, :, :] = value
+#     data[:, :, :, :, :, index, :, :] = value
+#     data[:, :, :, :, :, :, index, :] = value
+#     data[:, :, :, :, :, :, :, index] = value
+
+
+@njit("void(float32[::1], int64, int64, int64, float32)")
+def mask_jet(data, num_partons, max_jets, index, value):
+    if num_partons == 1:
+        mask_1(data, max_jets, index, value)
+    elif num_partons == 2:
+        mask_2(data, max_jets, index, value)
+    elif num_partons == 3:
         mask_3(data, max_jets, index, value)
     # elif num_partons == 4:
     #     mask_4(data, max_jets, index, value)
@@ -81,79 +122,6 @@ def mask_3(flat_data, size, index, value):
     # elif num_partons == 6:
     #     mask_6(data, max_jets, index, value)
     # elif num_partons == 7:
-    #     mask_7(data, max_jets, index, value)
-    # elif num_partons == 8:
-    #     mask_8(data, max_jets, index, value)
-
-
-@njit("int64[::1](int64, int64)")
-def compute_strides(num_partons, max_jets):
-    strides = np.zeros(num_partons, dtype=np.int64)
-    strides[-1] = 1
-    for i in range(num_partons - 2, -1, -1):
-        strides[i] = strides[i + 1] * max_jets
-
-    return strides
-
-
-@njit(TInt64[::1](TInt64, TInt64[::1]))
-def unravel_index(index, strides):
-    num_partons = strides.shape[0]
-    result = np.zeros(num_partons, dtype=np.int64)
-
-    remainder = index
-    for i in range(num_partons):
-        result[i] = remainder // strides[i]
-        remainder %= strides[i]
-    return result
-
-
-@njit(TInt64(TInt64[::1], TInt64[::1]))
-def ravel_index(index, strides):
-    return (index * strides).sum()
-
-
-@njit(numba.types.Tuple((TInt64, TInt64, TFloat32))(TPrediction))
-def maximal_prediction(predictions):
-    best_jet = -1
-    best_prediction = -1
-    best_value = -np.float32(np.inf)
-
-    for i in range(len(predictions)):
-        max_jet = np.argmax(predictions[i])
-        max_value = predictions[i][max_jet]
-
-        if max_value > best_value:
-            best_prediction = i
-            best_value = max_value
-            best_jet = max_jet
-
-    return best_jet, best_prediction, best_value
-
-
-@njit(numba.types.Tuple((TIResult, TFResult))(TPrediction, TInt64[::1], TInt64))
-def extract_prediction(predictions, num_partons, max_jets):
-    float_negative_inf = -np.float32(np.inf)
-    max_partons = num_partons.max()
-    num_targets = len(predictions)
-
-    # Create copies of predictions for safety and calculate the output shapes
-    strides = []
-    for i in range(num_targets):
-        strides.append(compute_strides(num_partons[i], max_jets))
-
-    # Fill up the prediction matrix
-    # -2 : Not yet assigned
-    # -1 : Masked value
-    # else : The actual index value
-    results = np.zeros((num_targets, max_partons), np.int64) - 2
-    results_weights = np.zeros(num_targets, dtype=np.float32) - np.float32(np.inf)
-
-    for _ in range(num_targets):
-        best_jet, best_prediction, best_value = maximal_prediction(predictions)
-
-        if not np.isfinite(best_value):
-            return results, results_weights
 
         best_jets = unravel_index(best_jet, strides[best_prediction])
 
