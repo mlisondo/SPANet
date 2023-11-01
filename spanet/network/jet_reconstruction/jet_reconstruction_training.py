3f2663a30dbf66664cb771d9058a88fa0a7a8d50
ereinha <74273926+ereinha@users.noreply.github.com>
2023-10-31 19:27:35 -0500

    Update jet_reconstruction_training.py

--- spanet/network/jet_reconstruction/jet_reconstruction_training.py
+++ spanet/network/jet_reconstruction/jet_reconstruction_training.py
@@ -70,6 +70,7 @@ class JetReconstructionTraining(JetReconstructionNetwork):
                         N = jet_indices.size(0) // where.size(0)
                         index_tensor = torch.zeros((N, where.size(0), 3), dtype=int, device=where.device)
                         index_tensor[:, batch_indices, :] = jet_indices
+                        print(target.size())
                         target_tensor = torch.stack(target, dim=0)
                         target_exp = target_tensor[:, :, None, :]
                         index_tensor_exp = index_tensor[None, ...]
