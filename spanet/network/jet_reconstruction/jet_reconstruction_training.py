fa3682339fd25ea80908e053fd1c49fc77879fe8
ereinha <74273926+ereinha@users.noreply.github.com>
2023-11-11 21:19:26 -0600

    Update jet_reconstruction_training.py

--- spanet/network/jet_reconstruction/jet_reconstruction_training.py
+++ spanet/network/jet_reconstruction/jet_reconstruction_training.py
@@ -85,7 +85,7 @@ class JetReconstructionTraining(JetReconstructionNetwork):
     
     def compute_symmetric_losses(self, assignments: Tuple[torch.Tensor], detections: List[torch.Tensor], targets: Tuple[Tuple[torch.Tensor]]):
         num_iterations = 2
-        print(len(self.event_permutation_tensor.cpu().numpy()))
+        print(self.event_permutation_tensor.cpu().numpy())
         symmetric_losses = []
         for iteration in range(num_iterations):
             for permutation in self.event_permutation_tensor.cpu().numpy():
