5c88831beb7dedf95a2056ca58f6b8e117f7883d
ereinha <74273926+ereinha@users.noreply.github.com>
2023-12-02 08:41:48 -0600

    Update jet_reconstruction_training.py

--- spanet/network/jet_reconstruction/jet_reconstruction_training.py
+++ spanet/network/jet_reconstruction/jet_reconstruction_training.py
@@ -107,9 +107,10 @@ class JetReconstructionTraining(JetReconstructionNetwork):
         # combined_loss, _ = symmetric_losses.min(0)
         num_iterations = 2
         symmetric_losses_reshaped = symmetric_losses.view(num_iterations, -1, symmetric_losses.size(-3), symmetric_losses.size(-2), symmetric_losses.size(-1))
-        symmetric_losses_reduced = torch.sqrt(symmetric_losses_reshaped.exp().prod(axis=0))
+        symmetric_losses_reduced = torch.sqrt(symmetric_losses_reshaped.exp().prod(dim=0))
         print(symmetric_losses_reduced)
-        total_symmetric_loss = torch.log(torch.sqrt(symmetric_losses_reduced.prod((1, 2), keepdim=False)))
+        total_symmetric_loss = torch.log(torch.sqrt(symmetric_losses_reduced.prod((dim=1))))
+        total_symmetric_loss = total_symmetric_loss.sum(1)
         print(total_symmetric_loss)
         index = total_symmetric_loss.argmin(0)
 
