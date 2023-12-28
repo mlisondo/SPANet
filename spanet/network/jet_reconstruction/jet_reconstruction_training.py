8c0ccbeb0516338530372448a1942edcecda433f
ereinha <74273926+ereinha@users.noreply.github.com>
2023-12-28 10:29:55 -0600

    Update jet_reconstruction_training.py

--- spanet/network/jet_reconstruction/jet_reconstruction_training.py
+++ spanet/network/jet_reconstruction/jet_reconstruction_training.py
@@ -81,11 +81,9 @@ class JetReconstructionTraining(JetReconstructionNetwork):
     
     def compute_symmetric_losses(self, assignments: Tuple[torch.Tensor], detections: List[torch.Tensor], targets: Tuple[Tuple[torch.Tensor]]):
         both_masks = []
-        masks = []
-        print('permutations: ', self.event_permutation_tensor.cpu().numpy())
         for permutation in self.event_permutation_tensor.cpu().numpy():
+            masks = []
             for _, mask in targets[permutation]:
-                print('mask: ', mask)
                 masks.append(mask)
             both_masks.append(torch.stack(masks))
         
