7eaeb28368512931e074936d01247b9d5d12c5a0
ereinha <74273926+ereinha@users.noreply.github.com>
2024-01-24 21:41:03 -0600

    Update divergence_losses.py

--- spanet/network/utilities/divergence_losses.py
+++ spanet/network/utilities/divergence_losses.py
@@ -1,10 +1,37 @@
 import torch
-from torch import Tensor
+from torch import Tensor, List
 from torch.nn import functional as F
 
+@torch.jit.script
+def unravel_index(indices: Tensor, shape: List[int]):
+    r = torch.floor_divide(indices, shape[1] * shape[2])
+    c = torch.floor_divide(indices % (shape[1] * shape[2]), shape[2])
+    d = indices % shape[2]
+    return r, c, d
 
 @torch.jit.script
-def assignment_cross_entropy_loss(prediction: Tensor, target_data: Tensor, target_mask: Tensor, gamma: float) -> Tensor:
+def find_and_extract_max_values(tensor: Tensor):
+    # Find the maximum values along the last three axes
+    max_vals = tensor.max(dim=-1, keepdim=True)[0].max(dim=-2, keepdim=True)[0].max(dim=-3, keepdim=True)[0]
+
+    # Create a mask for the max values and mask other values with -inf
+    mask = tensor == max_vals
+    masked_tensor = torch.where(mask, tensor, torch.tensor(float('-inf')))
+
+    # Compute the argmax along the last three axes
+    flat_dim = masked_tensor.shape[1] * masked_tensor.shape[2] * masked_tensor.shape[3]
+    flat_argmax = masked_tensor.reshape(tensor.shape[0], flat_dim).argmax(dim=-1)
+
+    # Convert flat indices to 3D indices
+    idx_3d = unravel_index(flat_argmax, tensor.shape[1:])
+
+    # Extract the values using these indices
+    max_values = tensor[torch.arange(tensor.shape[0]), idx_3d[0], idx_3d[1], idx_3d[2]]
+
+    return max_values
+
+@torch.jit.script
+def assignment_cross_entropy_loss(prediction: Tensor, target_data: Tensor, target_mask: Tensor, double_mask: Tensor, gamma: float) -> Tensor:
     batch_size = prediction.shape[0]
     prediction_shape = prediction.shape[1:]
 
@@ -26,9 +53,16 @@ def assignment_cross_entropy_loss(prediction: Tensor, target_data: Tensor, targe
     log_probability = ravel_prediction.gather(-1, ravel_target.view(-1, 1)).squeeze()
     log_probability = log_probability.masked_fill(~target_mask, 0.0)
 
-    focal_scale = (1 - torch.exp(log_probability)) ** gamma
+    pos_focal = -log_probability * (1 - torch.exp(log_probability)) ** gamma
+    log_prob_2 = find_and_extract_max_values(prediction)
+    log_prob_2 = log_prob_2.masked_fill(~target_mask, 0.0)
+    pos_focal_2 = -log_prob_2 * (1 - torch.exp(log_prob_2)) ** gamma
+    # neg_log_prob = prediction[max_loc]
+    # neg_prob = torch.exp(neg_log_prob)
+    # neg_focal = -torch.pow(neg_prob, gamma) * torch.log(1 - neg_prob)
+    # neg_focal = neg_focal.masked_fill(~target_mask, 0.0)
 
-    return -log_probability * focal_scale
+    return torch.where(double_mask, pos_focal_2, pos_focal)
 
 
 @torch.jit.script
