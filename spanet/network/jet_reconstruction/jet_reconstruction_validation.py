c7889bb01ee5ad8f3245f7d223aec73d9f44c945
ereinha <74273926+ereinha@users.noreply.github.com>
2025-06-09 22:59:47 -0500

    Remove deprecated np.bool
    Replace np.bool with bool

--- spanet/network/jet_reconstruction/jet_reconstruction_validation.py
+++ spanet/network/jet_reconstruction/jet_reconstruction_validation.py
@@ -40,8 +40,8 @@ class JetReconstructionValidation(JetReconstructionNetwork):
         # Compute all possible target permutations and take the best performing permutation
         # First compute raw_old accuracy so that we can get an accuracy score for each event
         # This will also act as the method for choosing the best permutation to compare for the other metrics.
-        jet_accuracies = np.zeros((num_permutations, num_targets, batch_size), dtype=np.bool)
-        particle_accuracies = np.zeros((num_permutations, num_targets, batch_size), dtype=np.bool)
+        jet_accuracies = np.zeros((num_permutations, num_targets, batch_size), dtype=bool)
+        particle_accuracies = np.zeros((num_permutations, num_targets, batch_size), dtype=bool)
         for i, permutation in enumerate(event_permutation_group):
             for j, (prediction, target) in enumerate(zip(jet_predictions, stacked_targets[permutation])):
                 jet_accuracies[i, j] = np.all(prediction == target, axis=1)
@@ -96,7 +96,7 @@ class JetReconstructionValidation(JetReconstructionNetwork):
 
         # Stack all of the targets into single array, we will also move to numpy for easier the numba computations.
         stacked_targets = np.zeros(num_targets, dtype=object)
-        stacked_masks = np.zeros((num_targets, batch_size), dtype=np.bool)
+        stacked_masks = np.zeros((num_targets, batch_size), dtype=bool)
         for i, (target, mask) in enumerate(targets):
             stacked_targets[i] = target.detach().cpu().numpy()
             stacked_masks[i] = mask.detach().cpu().numpy()
