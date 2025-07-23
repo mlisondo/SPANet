ac71b19eaaac6cdf07f5b251bc413dd5acb614ce
mlisondo <mlisondo@slurm-login.virtual.cms.physics.ua.edu>
2025-07-23 14:02:48 -0500

    first implementation of evaluation_scm

---
+++ spanet/evaluation_scm.py
@@ -0,0 +1,123 @@
+from glob import glob
+from typing import Optional, Union, Tuple
+
+import numpy as np
+import torch
+from torch.utils._pytree import tree_map
+
+from rich import progress
+
+import time
+
+from spanet.options import Options
+from spanet.network.jet_reconstruction.jet_scm_eval_test import SCM_Eval_Test
+
+from collections import defaultdict
+
+class Timer:
+    def __init__(self):
+        self.start_time = 0
+        self.total_time = 0
+        self.running = False
+
+    def start(self):
+        if not self.running:
+            self.start_time = time.time()
+            self.running = True
+
+    def stop(self):
+        if self.running:
+            self.total_time += time.time() - self.start_time
+            self.running = False
+
+    def reset(self):
+        self.start_time = 0
+        self.total_time = 0
+        self.running = False
+
+    def get_time(self):
+        if self.running:
+            return self.total_time + time.time() - self.start_time
+        else:
+            return self.total_time
+        
+
+
+
+def load_model(
+    log_directory: str,
+    testing_file: Optional[str] = None,
+    event_info_file: Optional[str] = None,
+    batch_size: Optional[int] = None,
+    cuda: bool = False,
+    checkpoint: Optional[str] = None
+) -> SCM_Eval_Test:                                                                       # CHANGED
+    # Load the best-performing checkpoint on validation data
+    if checkpoint is None:
+        checkpoint = sorted(glob(f"{log_directory}/checkpoints/epoch*"))[-1]
+        print(f"Loading: {checkpoint}")
+
+    checkpoint = torch.load(checkpoint, map_location='cpu')
+    checkpoint = checkpoint["state_dict"]
+
+    # Load the options that were used for this run and set the testing-dataset value
+    options = Options.load(f"{log_directory}/options.json")
+
+    # Override options from command line arguments
+    if testing_file is not None:
+        options.testing_file = testing_file
+
+    if event_info_file is not None:
+        options.event_info_file = event_info_file
+
+    if batch_size is not None:
+        options.batch_size = batch_size
+
+    # Create model and disable all training operations for speed
+    model = SCM_Eval_Test(options)                                                                       # CHANGED
+    model.load_state_dict(checkpoint)
+    model = model.eval()
+    for parameter in model.parameters():
+        parameter.requires_grad_(False)
+
+    if cuda:
+        model = model.cuda()
+
+    return model
+
+
+def evaluate_on_test_dataset(
+        model: SCM_Eval_Test,
+        progress=progress,
+        return_full_output: bool = False
+) -> Union[dict, Tuple[dict, Tuple[dict, ...]]]:
+    # Loops over model.test_dataloader(), calls model.evaluate_scm_batch(batch)     (which runs extract_predictions internally)
+
+    bank = defaultdict(list)
+    full_outputs = []
+
+    dataloader = model.test_dataloader()
+    if progress:
+        dataloader = progress.track(dataloader, description="Evaluating SCM model")
+
+    timer = Timer()
+    for batch in dataloader:
+        batch = batch.to(model.device)
+
+        timer.start()
+        out = model.evaluate_scm_batch(batch)
+        timer.stop()
+
+        for k, v in out.items():
+            bank[k].append(v.cpu().numpy())
+
+        if return_full_output:
+            full_outputs.append(tree_map(lambda x: x.cpu().numpy(), out))
+
+    print(f"Total combinatorics time: {timer.get_time():.2f} s")
+
+    arrays = {k: np.concatenate(v, axis=0) for k, v in bank.items()}
+
+    if return_full_output:
+        return arrays, tuple(full_outputs)
+    return arrays
