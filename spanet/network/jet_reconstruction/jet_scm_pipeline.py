492add7e67cafae7b15806d0ac20d6cde51c1055
Mateo Lisondo <mateo.lisondo@upr.edu>
2025-07-29 19:18:18 -0500

    removed probes

--- spanet/network/jet_reconstruction/jet_scm_pipeline.py
+++ spanet/network/jet_reconstruction/jet_scm_pipeline.py
@@ -150,52 +150,52 @@ class JetSecondaryLoader(JetReconstructionNetwork):
 
         one_one = torch.cat([super_true_event_idx, true_event_idx, false_event_idx])  
 
-        probe(sources[0], "sources[0]")
-        probe(jet_data, "jet_data")
-        probe(jet_preds_tensor, "jet_preds_tensor")
-        probe(true_idx, "true_idx")
-        probe(true_masks, "true_masks")
-        probe(pred_truth, "pred_truth")
-        probe(class_truth, "class_truth")
-        probe(features_arr, "features_arr")
-        probe(jet_data, "jet_data")
-        probe(jet_preds_tensor, "jet_preds_tensor")
-        probe(canon_idx, "canon_idx")
-        probe(canon_masks, "canon_masks")
+        # probe(sources[0], "sources[0]")
+        # probe(jet_data, "jet_data")
+        # probe(jet_preds_tensor, "jet_preds_tensor")
+        # probe(true_idx, "true_idx")
+        # probe(true_masks, "true_masks")
+        # probe(pred_truth, "pred_truth")
+        # probe(class_truth, "class_truth")
+        # probe(features_arr, "features_arr")
+        # probe(jet_data, "jet_data")
+        # probe(jet_preds_tensor, "jet_preds_tensor")
+        # probe(canon_idx, "canon_idx")
+        # probe(canon_masks, "canon_masks")
 
-        for e in one_one:
-            print(f"\n===== EVENT {int(e)} =====")
+        # for e in one_one:
+        #     print(f"\n===== EVENT {int(e)} =====")
 
-            print("Jet data for events:")
-            print(jet_data[e])
+        #     print("Jet data for events:")
+        #     print(jet_data[e])
 
-            print("jet_preds_tensor:")
-            print(jet_preds_tensor[e])
+        #     print("jet_preds_tensor:")
+        #     print(jet_preds_tensor[e])
 
-            print("true_idx:")
-            print(true_idx[:, e])
+        #     print("true_idx:")
+        #     print(true_idx[:, e])
 
-            print("canon_idx:")
-            print(canon_idx[:, e])
+        #     print("canon_idx:")
+        #     print(canon_idx[:, e])
 
-            print("true_masks:")
-            print(true_masks[:, e])
+        #     print("true_masks:")
+        #     print(true_masks[:, e])
 
-            print("canon_masks")
-            print(canon_masks[:, e])
+        #     print("canon_masks")
+        #     print(canon_masks[:, e])
 
-            print("pred_truth matrix (K x B):")
-            print(pred_truth[e])
+        #     print("pred_truth matrix (K x B):")
+        #     print(pred_truth[e])
 
-            print("class_truth row:")
-            print(class_truth[e])
+        #     print("class_truth row:")
+        #     print(class_truth[e])
 
-            print("feature for selected events:")
-            print(features_arr[e])
+        #     print("feature for selected events:")
+        #     print(features_arr[e])
 
-            print("=" * 30)
+        #     print("=" * 30)
 
-        raise RuntimeError("Debug break")
+        # raise RuntimeError("Debug break")
 
         return pred_truth, canon_masks, features_arr, class_truth, canon_idx, jet_preds_tensor
 
