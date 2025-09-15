5e7571de6d62800b453ab138426fc27a981287d9
ereinha <74273926+ereinha@users.noreply.github.com>
2025-09-15 09:45:52 -0500

    Update jet_scm_training_validation.py

--- spanet/network/jet_reconstruction/jet_scm_training_validation.py
+++ spanet/network/jet_reconstruction/jet_scm_training_validation.py
@@ -80,8 +80,7 @@ class ClassifierTransformerHead(nn.Module):
     def forward(self, features_arr, valid_mask: torch.Tensor | None = None,
                 zero_out_invalid: bool = True):
         # features_arr: (N, K, B, J, F)
-        momentum_indices = [1,3,4]
-        tertiary_features_arr = features_arr[...,momentum_indices]
+        tertiary_features_arr = features_arr[...,:3]
         N, K, B, J, Fdim = features_arr.shape
         device = features_arr.device
     
