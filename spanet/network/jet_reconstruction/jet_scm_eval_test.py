49cc5d8baf9701ac8f7ca8b24ea1e09e18d1a910
Mateo Lisondo <mateo.lisondo@upr.edu>
2025-10-30 20:56:50 -0400

    changed for set

--- spanet/network/jet_reconstruction/jet_scm_eval_test.py
+++ spanet/network/jet_reconstruction/jet_scm_eval_test.py
@@ -23,12 +23,15 @@ class SCM_Eval_Test(SCM_Training_Val):
         prior_ft = features_arr[..., :3].contiguous()
 
         # ================== masker ==================
-        # masker will be avoided for now
         (inclusive_bt, inclusive_ct, inclusive_mask_logits, 
         prior_bt, prior_ct, prior_mask_logits) = self.masker(
             inclusive_X = inclusive_ft, prior_X = prior_ft,
             inclusive_branch_kpm = branch_kpm, prior_branch_kpm = branch_kpm
         )
+
+        mask_logits = prior_mask_logits * 2 + inclusive_mask_logits     # NOTE: THIS IS A WEIGHTED GUESS, LOSS FUNCTION IS NOT AS BIASED.
+        mask_probs = torch.sigmoid(mask_logits)
+        mask_preds = (mask_prob > 0.5).long()
 
         # ================== classifier ==================
         (inclusive_logits, inclusive_ct, global_inclusive,
@@ -38,4 +41,25 @@ class SCM_Eval_Test(SCM_Training_Val):
             branch_kpm_inclusive = branch_kpm, branch_kpm_prior = branch_kpm
         )
 
+        class_logits = prior_logits * 2 + inclusive_logits     # NOTE: THIS IS A WEIGHTED GUESS, LOSS FUNCTION IS NOT BIASED.
         neg_inf = torch.tensor(float("-inf"), device = inclusive_logits, dtype = inclusive_logits)
+        masked_logits = class_logits.masked_fill(valid_mask, neg_inf)
+        class_probs = torch.softmax(masked_logits, dim=1)  # sums to 1 over valid K
+        class_preds = class_probs.argmax(dim=1)
+
+        raw_valid = valid_mask.any(dim=-1)  # (E,)
+
+        return {
+            "class_logits": class_logits,   # CL
+            "class_probs":  class_probs,    # CP (zeros on duplicates)
+            "class_preds":  class_preds,    # CPd
+            "mask_logits":  mask_logits,    # ML
+            "mask_probs":   mask_probs,     # MP
+            "mask_preds":   mask_preds,     # MPd
+            "class_truth":  class_truth,    # CT
+            "pred_truth":   pred_truth,     # PT
+            "features_arr": features_arr,   # FA
+            "true_masks":   valid_mask,     # TM
+            "raw_valid":    raw_valid,      # RV
+            "jet_mult":     jet_mult,       # JM
+        }
